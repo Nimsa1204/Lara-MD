f08ebd755a0e0e0343e49e04645a46d7d02edce5
Nimsa1204 <oldnima7@gmail.com>
2024-12-21 08:29:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,6 @@ MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
-AUTO_REACT: process.env.AUTO_REACT || "true",
+AUTO_REACT: process.env.AUTO_REACT || "false",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",    
 };
